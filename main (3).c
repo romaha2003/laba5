d3fe7d71de5f9e2ffa42d9494a977e1608df2229
Martelsss <94805176+Martelsss@users.noreply.github.com>
2021-11-21 23:36:48 +0200

    Update main (3).c

--- main (3).c
+++ main (3).c
@@ -1,10 +1,8 @@
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 int main()
-{
-    printf("Hello World");
-
-    return 0;
-}
-fsbfbsfnsfn
+ int dim, min, max;
+ float A[14]
