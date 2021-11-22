812612da322799d952f13b887a0994f2c5fdebcd
Andriy3131 <94804850+Andriy3131@users.noreply.github.com>
2021-11-22 12:25:37 +0200

    Add files via upload

---
+++ Гундерчук.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int main(){
+ int dim, min, max;
+float A[14];
+ 
+
+ 
+int Nneg=0,Npos=0;
+for (int i =0; i<dim; i++){
+    if(array[i]<0){
+    Nneg++;
+    }
+    if(array[i]>0){
+    Npos++;
+    }
+}
+
+
+if(Nneg=14){
+    for (int i =0; i<dim; i++){
+    array[i]=0;    
+    }
+}
+else if(Npos=14){
+    for (int i =0; i<14; i++){
+    array[i]=1;    
+    }    
+}    
+    
+else if{
+    
+}   
+    return 0;
+}
